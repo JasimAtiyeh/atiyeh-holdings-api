f43e542875d2270531d20ac1453cc0562281563a
Jasim Atiyeh <jasimatiyeh@gmail.com>
2024-05-16 22:19:42 -0500

    Add MongoDB calls for houses. UNTESTED!

--- src/repos/houses.ts
+++ src/repos/houses.ts
@@ -1,7 +1,8 @@
-import { Collection, ObjectId } from "mongodb";
+import { Collection, InsertOneResult, ObjectId, UpdateResult } from "mongodb";
 import { GetCollection } from ".";
 import "dotenv/config";
-import House from "../models/houses";
+import House, { HouseDetails, Lease } from "../models/houses";
+import { UserTypes } from "../models/users";
 
 let houseCollection: Collection<House>;
 
@@ -11,12 +12,88 @@ export async function ConnectHouseCollection() {
   );
 }
 
-async function GetHouse(houseId: ObjectId): Promise<House | null> {
-  const house = await houseCollection.findOne<House>({ _id: houseId });
+// Get House By Id
+async function GetHouseById(houseId: string): Promise<House | null> {
+  const house = await houseCollection.findOne<House>({
+    _id: new ObjectId(houseId),
+  });
   if (!house) return null;
   return house;
 }
 
-// GetHouses
-// UpdateHouse
-// DeleteHouse
+// Get Houses For Owner
+async function GetHousesForOwner(userId: string): Promise<House[] | null> {
+  const houses = await houseCollection
+    .find<House>({
+      users: { _id: userId },
+    })
+    .toArray();
+  if (!houses) return null;
+  return houses;
+}
+
+// Get House For Tenant
+async function GetHouseForTenant(userId: string): Promise<House | null> {
+  const house = await houseCollection.findOne<House>({
+    lease: { tenantId: userId },
+  });
+  if (!house) return null;
+  return house;
+}
+
+// Get Houses
+async function GetHouses(): Promise<House[] | null> {
+  const houses = await houseCollection.find<House>({}).toArray();
+  if (!houses.length) return null;
+  return houses;
+}
+
+// Create House
+async function CreateHouse(
+  address: string,
+  name: string,
+  ownerIds: Array<ObjectId>,
+  details?: HouseDetails,
+  lease?: Lease
+): Promise<InsertOneResult<House>> {
+  const houseCreated = await houseCollection.insertOne({
+    address,
+    name,
+    ownerIds,
+    details,
+    lease,
+  });
+  return houseCreated;
+}
+
+// Update House
+async function UpdateHouse(
+  houseId: string,
+  updateData: Partial<House>
+): Promise<boolean> {
+  const houseUpdated = await houseCollection.updateOne(
+    { _id: new ObjectId(houseId) },
+    { $set: updateData }
+  );
+  const { acknowledged, modifiedCount } = houseUpdated;
+  if (!acknowledged && modifiedCount === 0) return false;
+  return true;
+}
+
+// Delete House
+async function DeleteHouse(houseId: string): Promise<boolean> {
+  const houseDeleted = await houseCollection.deleteOne({
+    _id: new ObjectId(houseId),
+  });
+  return houseDeleted.acknowledged;
+}
+
+export {
+  GetHouseById,
+  GetHousesForOwner,
+  GetHouseForTenant,
+  GetHouses,
+  CreateHouse,
+  UpdateHouse,
+  DeleteHouse,
+};
