4fc1183cd2d82d9c1433b427daaa365fedb9594c
Jasim Atiyeh <jasimatiyeh@gmail.com>
2024-05-17 22:42:10 -0500

    Move validators to models

--- src/repos/index.ts
+++ src/repos/index.ts
@@ -1,7 +1,22 @@
 import { MongoClient, Db, Collection, Document as Doc } from "mongodb";
 import "dotenv/config";
+import { ConnectUserCollection } from "./users";
+import { ConnectHouseCollection } from "./houses";
+import { ConnectLeaseCollection } from "./leases";
+import { ConnectTicketCollection } from "./tickets";
+import { usersSchemaValidator } from "../models/users";
+import { housesSchemaValidator } from "../models/houses";
+import { leasesSchemaValidator } from "../models/leases";
+import { ticketsSchemaValidator } from "../models/tickets";
 
-const { MONGO_CONNECTION_STRING, MONGO_DB: dbName } = process.env;
+const {
+  MONGO_CONNECTION_STRING,
+  MONGO_DB,
+  USER_COLLECTION,
+  HOUSE_COLLECTION,
+  LEASE_COLLECTION,
+  TICKET_COLLECTION,
+} = process.env;
 
 let db: Db;
 
@@ -11,13 +26,14 @@ const client = new MongoClient(MONGO_CONNECTION_STRING as string);
 export async function ConnectDB() {
   try {
     await client.connect();
-    db = client.db(dbName);
-    await UsersSchema();
-    // await HousesSchema();
+    db = client.db(MONGO_DB, { promoteValues: true });
+    await ConnectUserCollection();
+    await ConnectHouseCollection();
+    await ConnectLeaseCollection();
+    await ConnectTicketCollection();
     console.log("Successfully connected to MongoDB!");
   } catch (error) {
     console.error(error);
-  } finally {
     await client.close();
   }
 }
@@ -25,112 +41,15 @@ export async function ConnectDB() {
 export async function GetCollection<T extends Doc>(
   collectionName: string
 ): Promise<Collection<T>> {
-  return (await client.connect())
-    .db(process.env.MONGO_DB as string)
-    .collection<T>(collectionName);
+  db.createCollection<T>(collectionName, {
+    validator: schemaValidators[collectionName],
+  });
+  return db.collection<T>(collectionName);
 }
 
-async function UsersSchema() {
-  await db.command({
-    collMod: process.env.USER_COLLECTION,
-    validator: {
-      $jsonSchema: {
-        bsonType: "object",
-        required: ["name", "email", "password", "role"],
-        additionalProperties: false,
-        properties: {
-          _id: {},
-          name: {
-            bsonType: "string",
-            description: "'name' is required and is a string",
-          },
-          email: {
-            bsonType: "string",
-            description: "'email' is required and is a string",
-          },
-          password: {
-            bsonType: "string",
-            description: "'password' is required and is a string",
-          },
-          houses: {
-            bsonType: "array",
-            description: "array of houses associated with the user",
-          },
-          role: {
-            bsonType: "string",
-            description:
-              "'role' is required and is either 'admin', 'owner', or 'tentant",
-          },
-        },
-      },
-    },
-  });
-}
-
-async function HousesSchema() {
-  await db.command({
-    collMod: process.env.HOUSE_COLLECTION,
-    validator: {
-      $jsonSchema: {
-        bsonType: "object",
-        required: ["name", "address"],
-        additionalProperties: false,
-        properties: {
-          _id: {},
-          name: {
-            bsonType: "string",
-            description: "'name' is required and is a string",
-          },
-          address: {
-            bsonType: "string",
-            description: "'address' is required and is a string",
-          },
-          ownerIds: {
-            bsonType: "array",
-            description: "array of user ids associated with the house",
-          },
-          details: {
-            bsonType: "object",
-            properties: {
-              bathrooms: {
-                bsonType: "number",
-              },
-              bedrooms: {
-                bsonType: "number",
-              },
-              currentValue: {
-                bsonType: "number",
-              },
-              purchasePrice: {
-                bsonType: "number",
-              },
-              sqft: {
-                bsonType: "number",
-              },
-            },
-          },
-          lease: {
-            bsonType: "object",
-            properties: {
-              deposit: {
-                bsonType: "number",
-              },
-              end: {
-                bsonType: "date",
-              },
-              rentPrice: {
-                bsonType: "number",
-              },
-              start: {
-                bsonType: "date",
-              },
-              tenantId: {
-                bsonType: "string",
-              },
-            },
-          },
-        },
-      },
-    },
-  });
-}
+const schemaValidators = {
+  [USER_COLLECTION as string]: usersSchemaValidator,
+  [HOUSE_COLLECTION as string]: housesSchemaValidator,
+  [LEASE_COLLECTION as string]: leasesSchemaValidator,
+  [TICKET_COLLECTION as string]: ticketsSchemaValidator,
+};
